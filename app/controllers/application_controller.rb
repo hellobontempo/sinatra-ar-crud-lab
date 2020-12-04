80514a2dc973846b18190068959187cbfaad9180
claire <clairebontempo@gmail.com>
2020-12-03 18:10:55 -0800

    commit tree 0ba482ff4d25301fc0417a703f30b98851adde6e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,50 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+  #index
+  get '/articles' do 
+    @articles = Article.all
+    erb :index
+  end
+
+  #new
+  get '/articles/new' do
+    @article = Article.new
+    erb :new
+  end
+
+  #create
+  post '/articles' do
+    article = Article.create(params)
+    redirect "/articles/#{article.id}"
+  end
+
+  #show
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  #edit 
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  #patch
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect "/articles/#{@article.id}"
+  end
+  
+  delete '/articles/:id' do
+    article = Article.find(params[:id])
+    article.destroy
+    redirect '/'
+  end
+
+
 end
+
